67345f84ecc36a65410507b2322df01fbfea2ad9
ckswnskfk <ckswnskfk@hanmail.net>
2024-09-15 23:49:08 +0900

    chore: lint 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,58 +1,59 @@
 import { EnvVarWarning } from "@/components/env-var-warning";
 import HeaderAuth from "@/components/header-auth";
 import { ThemeSwitcher } from "@/components/theme-switcher";
-import { hasEnvVars } from "@/supabase/check-env-vars";
 import { GeistSans } from "geist/font/sans";
-import { ThemeProvider } from "next-themes";
-import Link from "next/link";
 import "./globals.css";
+import { hasEnvVars } from "@/supabase/check-env-vars";
+import Link from "next/link";
+import { ThemeProvider } from "next-themes";
 
 const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+	? `https://${process.env.VERCEL_URL}`
+	: "http://localhost:3000";
 
 export const metadata = {
-  metadataBase: new URL(defaultUrl),
-  title: "On-Lesson",
-  description: "거리 제한 없는 세상의 모든 레슨",
+	metadataBase: new URL(defaultUrl),
+	title: "On-Lesson",
+	description: "거리 제한 없는 세상의 모든 레슨",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en" className={GeistSans.className} suppressHydrationWarning>
-      <body className="bg-background text-foreground">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange>
-          <main className="min-h-screen flex flex-col items-center">
-            <div className="flex-1 w-full flex flex-col gap-20 items-center">
-              <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-                <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-                  <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"} className="text-lg text-blue-500">
-                      On-Lesson
-                    </Link>
-                  </div>
-                  {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
-                </div>
-              </nav>
-              <div className="flex flex-col gap-20 max-w-5xl p-5">
-                {children}
-              </div>
+	return (
+		<html lang="en" className={GeistSans.className} suppressHydrationWarning>
+			<body className="bg-background text-foreground">
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+					disableTransitionOnChange
+				>
+					<main className="min-h-screen flex flex-col items-center">
+						<div className="flex-1 w-full flex flex-col gap-20 items-center">
+							<nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+								<div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
+									<div className="flex gap-5 items-center font-semibold">
+										<Link href={"/"} className="text-lg text-blue-500">
+											On-Lesson
+										</Link>
+									</div>
+									{!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
+								</div>
+							</nav>
+							<div className="flex flex-col gap-20 max-w-5xl p-5">
+								{children}
+							</div>
 
-              <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-                <ThemeSwitcher />
-              </footer>
-            </div>
-          </main>
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+							<footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
+								<ThemeSwitcher />
+							</footer>
+						</div>
+					</main>
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
