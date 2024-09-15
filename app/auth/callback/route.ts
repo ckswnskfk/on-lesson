d19c697dce91549e05469433be6db9c587c7031a
ckswnskfk <ckswnskfk@hanmail.net>
2024-09-15 22:41:25 +0900

    feat: 카카오 로그인

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -2,21 +2,32 @@ import { createClient } from "@/utils/supabase/server";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  // The `/auth/callback` route is required for the server-side auth flow implemented
-  // by the SSR package. It exchanges an auth code for the user's session.
-  // https://supabase.com/docs/guides/auth/server-side/nextjs
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
   const origin = requestUrl.origin;
   const redirectTo = requestUrl.searchParams.get("redirect_to")?.toString();
+  const next = requestUrl.searchParams.get("next") ?? "/";
 
   if (code) {
     const supabase = createClient();
-    await supabase.auth.exchangeCodeForSession(code);
-  }
+    const { error } = await supabase.auth.exchangeCodeForSession(code);
 
-  if (redirectTo) {
-    return NextResponse.redirect(`${origin}${redirectTo}`);
+    if (!error && next) {
+      const forwardedHost = request.headers.get("x-forwarded-host");
+      const isLocalEnv = process.env.NODE_ENV === "development";
+
+      if (isLocalEnv) {
+        return NextResponse.redirect(`${origin}${next}`);
+      } else if (forwardedHost) {
+        return NextResponse.redirect(`https://${forwardedHost}${next}`);
+      } else {
+        return NextResponse.redirect(`${origin}${next}`);
+      }
+    }
+
+    if (!error && redirectTo) {
+      return NextResponse.redirect(`${origin}${redirectTo}`);
+    }
   }
 
   // URL to redirect to after sign up process completes
---
+++ components/oauth-buttons.tsx
@@ -0,0 +1,9 @@
+import KakaoButton from "./kakao-button";
+
+export default function OAuthButtons() {
+  return (
+    <div className="flex gap-[1rem]">
+      <KakaoButton />
+    </div>
+  );
+}
