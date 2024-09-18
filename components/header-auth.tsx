989398b35f3a6e354bbd704cdc20b801ba37a145
ckswnskfk <ckswnskfk@hanmail.net>
2024-09-18 23:16:24 +0900

    chore: 사용하지 않는 코드 삭제

--- components/header-auth.tsx
+++ components/header-auth.tsx
@@ -1,10 +1,8 @@
 import Link from "next/link";
 
 import { signOutAction } from "@/app/actions";
-import { hasEnvVars } from "@/supabase/check-env-vars";
 import { createClient } from "@/supabase/server";
 
-import { Badge } from "./ui/badge";
 import { Button } from "./ui/button";
 
 export default async function AuthButton() {
@@ -12,58 +10,22 @@ export default async function AuthButton() {
 		data: { user },
 	} = await createClient().auth.getUser();
 
-	if (!hasEnvVars) {
-		return (
-			<>
-				<div className="flex items-center gap-4">
-					<div>
-						<Badge
-							variant={"default"}
-							className="pointer-events-none font-normal"
-						>
-							Please update .env.local file with anon key and url
-						</Badge>
-					</div>
-					<div className="flex gap-2">
-						<Button
-							asChild
-							size="sm"
-							variant={"outline"}
-							disabled
-							className="pointer-events-none cursor-none opacity-75"
-						>
-							<Link href="/sign-in">Sign in</Link>
-						</Button>
-						<Button
-							asChild
-							size="sm"
-							variant={"default"}
-							disabled
-							className="pointer-events-none cursor-none opacity-75"
-						>
-							<Link href="/sign-up">Sign up</Link>
-						</Button>
-					</div>
-				</div>
-			</>
-		);
-	}
 	return user ? (
 		<div className="flex items-center gap-4">
-			Hey, {user.email}!
+			안녕하세요, {user.email}!
 			<form action={signOutAction}>
 				<Button type="submit" variant={"outline"}>
-					Sign out
+					로그아웃
 				</Button>
 			</form>
 		</div>
 	) : (
 		<div className="flex gap-2">
 			<Button asChild size="sm" variant={"outline"}>
-				<Link href="/sign-in">Sign in</Link>
+				<Link href="/sign-in">로그인</Link>
 			</Button>
 			<Button asChild size="sm" variant={"default"}>
-				<Link href="/sign-up">Sign up</Link>
+				<Link href="/sign-up">회원가입</Link>
 			</Button>
 		</div>
 	);
