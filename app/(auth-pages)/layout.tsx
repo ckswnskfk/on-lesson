7a5999f66a4a064ee83806d8b4d65cedbe3b9472
ckswnskfk <ckswnskfk@hanmail.net>
2024-09-20 16:28:26 +0900

    style: OAuth 버튼 스타일 변경

--- app/(auth-pages)/layout.tsx
+++ app/(auth-pages)/layout.tsx
@@ -1,9 +1,9 @@
 export default async function Layout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <div className="flex max-w-7xl flex-col items-start gap-12">{children}</div>
-  );
+	return (
+		<div className="flex max-w-7xl flex-col items-start gap-6">{children}</div>
+	);
 }
--- components/oauth-buttons.tsx
+++ components/oauth-buttons.tsx
@@ -1,9 +1,19 @@
 import KakaoButton from "./kakao-button";
 
 export default function OAuthButtons() {
-  return (
-    <div className="flex gap-4">
-      <KakaoButton />
-    </div>
-  );
+	return (
+		<div className="flex w-full flex-col gap-4">
+			<div className="relative">
+				<div className="absolute inset-0 flex items-center">
+					<div className="w-full border-t border-gray-300"></div>
+				</div>
+				<div className="relative flex justify-center text-sm">
+					<span className="bg-white px-3 text-sm text-foreground">or</span>
+				</div>
+			</div>
+			<div>
+				<KakaoButton />
+			</div>
+		</div>
+	);
 }
