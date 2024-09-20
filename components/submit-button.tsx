6ed69a53b64c108aca689b1aa64b774b903fe7ad
ckswnskfk <ckswnskfk@hanmail.net>
2024-09-20 16:12:08 +0900

    chore: 기본 pendingText 변경

--- components/submit-button.tsx
+++ components/submit-button.tsx
@@ -6,19 +6,19 @@ import { useFormStatus } from "react-dom";
 import { Button } from "@/components/ui/button";
 
 type Props = ComponentProps<typeof Button> & {
-  pendingText?: string;
+	pendingText?: string;
 };
 
 export function SubmitButton({
-  children,
-  pendingText = "Submitting...",
-  ...props
+	children,
+	pendingText = "처리중...",
+	...props
 }: Props) {
-  const { pending } = useFormStatus();
+	const { pending } = useFormStatus();
 
-  return (
-    <Button type="submit" aria-disabled={pending} {...props}>
-      {pending ? pendingText : children}
-    </Button>
-  );
+	return (
+		<Button type="submit" aria-disabled={pending} {...props}>
+			{pending ? pendingText : children}
+		</Button>
+	);
 }
