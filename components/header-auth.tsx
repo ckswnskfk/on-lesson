acdb6044f17eea06627ee463d2c1455ac53ebd5b
ckswnskfk <ckswnskfk@hanmail.net>
2024-09-25 15:51:28 +0900

    chore: 코드 스타일 통일

--- components/header-auth.tsx
+++ components/header-auth.tsx
@@ -6,13 +6,15 @@ import { createClient } from "@/supabase/server";
 import { Button } from "./ui/button";
 
 export default async function AuthButton() {
+	const supabase = createClient();
+
 	const {
 		data: { user },
-	} = await createClient().auth.getUser();
+	} = await supabase.auth.getUser();
 
 	return user ? (
 		<div className="flex items-center gap-4">
-			안녕하세요, {user.email}!
+			안녕하세요, {user.user_metadata?.user_name}!
 			<form action={signOutAction}>
 				<Button type="submit" variant={"outline"}>
 					로그아웃
