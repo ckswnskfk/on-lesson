208393bdf4d9d9cf5bcda5ea6a9aa455c0def2ee
ckswnskfk <ckswnskfk@hanmail.net>
2024-09-25 17:27:37 +0900

    chore: supabase cli를 통한 데이터베이스 타입 가져오기

--- lib/definitions.ts
+++ lib/definitions.ts
@@ -1,3 +1,8 @@
+import { Database } from "@/database.types";
+import { SupabaseClient } from "@supabase/supabase-js";
+
+export type TypedSupabaseClient = SupabaseClient<Database>;
+
 export enum Role {
 	STUDENT = "STUDENT",
 	TEACHER = "TEACHER",
@@ -9,3 +14,5 @@ export type User = {
 	email: string;
 	role: Role;
 };
+
+export type Lesson = Database["public"]["Tables"]["lessons"]["Row"];
